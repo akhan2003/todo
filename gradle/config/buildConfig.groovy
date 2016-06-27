abd9bce61c8f50af3906af40e04571ff661764b6
akhan2003 <Adnank2003@hotmail.com>
2016-06-27 17:21:44 -0500

    Update buildConfig.groovy

--- gradle/config/buildConfig.groovy
+++ gradle/config/buildConfig.groovy
@@ -1,8 +1,8 @@
 binaryRepository {
     baseUrl = 'http://localhost:8081/artifactory'
     releaseUrl = "$baseUrl/libs-release-local"
-    username = 'admin'
-    password = 'password'
+    username = "${artifactory_user}"    
+    password = "${artifactory_password}"    
 }
 
 environments {
